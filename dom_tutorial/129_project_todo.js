1eff3e08b4dde683b309da8c64596bed65790d73
Gaurav Kumar <gk956842@gmail.com>
2022-11-02 18:58:11 +0530

    last project 129.js

---
+++ dom_tutorial/129_project_todo.js
@@ -0,0 +1,39 @@
+
+
+const input = document.querySelector(".form-todo input[type='text']");
+const todoForm = document.querySelector(".form-todo");
+const todoList = document.querySelector(".todo-list");
+
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value === "") {
+        alert("Empty todo not allowed!!!");
+    } else {
+        const newTodo = document.createElement("li");
+        newTodo.innerHTML = `
+        <span class="text">${input.value}</span>
+        <div class="todo-buttons">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+        </div>
+    `
+        todoList.append(newTodo);
+        input.value = "";
+    }
+})  
+
+
+
+todoList.addEventListener("click",(e)=>{
+    //check if user clicked on done button
+    if(e.target.classList.contains("done")){
+        const lispan = e.target.parentNode.previousElementSibling;
+        lispan.style.textDecoration = "line-through"
+    }
+    if(e.target.classList.contains("remove")){
+        const li = e.target.parentNode.parentNode;
+        li.remove();
+    }
+
+})
