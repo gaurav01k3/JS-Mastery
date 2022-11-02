4daf50208ca282b66f250b336087b6f77dffea15
Gaurav Kumar <gk956842@gmail.com>
2022-11-02 16:32:06 +0530

    this.js modified

--- 72_this.js
+++ 72_this.js
@@ -9,3 +9,11 @@ function myFunc() {
 }
 
 myFunc();
+
+
+// In an object method, this refers to the object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+// Methods like call(), apply(), and bind() can refer this to any object.
